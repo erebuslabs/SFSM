a81ae18623acbca8b878e3f6c1538c756f31e225
borowczak@gmail.com <borowczak@gmail.com@a7cb938e-b4f2-1df4-7820-3bc43eb7a654>
2013-01-30 00:08:32 +0000

    Minor modification for reability

--- trunk/z3_py/2stagebp.py
+++ trunk/z3_py/2stagebp.py
@@ -1,6 +1,6 @@
 from z3 import *
 # Create to bit-vectors of size 32
-x, y ,z , w, p, n = BitVecs('x y z w p n',4)
+x, y, z, w, p, n = BitVecs('x y z w p n',8)
 
 s = Solver()
 
@@ -12,44 +12,44 @@ print s.check()
 #check that they all have the same number of bits turned on
 
 s.add(
-  Sum([((x & (2**(i)))/(2**(i))) for i in range(4)]) ==
-  Sum([((y & (2**(i)))/(2**(i))) for i in range(4)])
+  Sum([((x & (2**(i)))/(2**(i))) for i in range(8)]) ==
+  Sum([((y & (2**(i)))/(2**(i))) for i in range(8)])
 )
 s.add(
-  Sum([((x & (2**(i)))/(2**(i))) for i in range(4)]) ==
-  Sum([((z & (2**(i)))/(2**(i))) for i in range(4)]))
+  Sum([((x & (2**(i)))/(2**(i))) for i in range(8)]) ==
+  Sum([((z & (2**(i)))/(2**(i))) for i in range(8)]))
 s.add(
-  Sum([((x & (2**(i)))/(2**(i))) for i in range(4)]) ==
-  Sum([((w & (2**(i)))/(2**(i))) for i in range(4)])
+  Sum([((x & (2**(i)))/(2**(i))) for i in range(8)]) ==
+  Sum([((w & (2**(i)))/(2**(i))) for i in range(8)])
 )
 s.add(
-  Sum([((x & (2**(i)))/(2**(i))) for i in range(4)]) ==
-  Sum([((p & (2**(i)))/(2**(i))) for i in range(4)])
+  Sum([((x & (2**(i)))/(2**(i))) for i in range(8)]) ==
+  Sum([((p & (2**(i)))/(2**(i))) for i in range(8)])
 )
 s.add(
-  Sum([((x & (2**(i)))/(2**(i))) for i in range(4)]) ==
-  Sum([((n & (2**(i)))/(2**(i))) for i in range(4)])
+  Sum([((x & (2**(i)))/(2**(i))) for i in range(8)]) ==
+  Sum([((n & (2**(i)))/(2**(i))) for i in range(8)])
 )
       
 
-#print Sum([(((x^y) & (2**(i)))/(2**(i))) for i in range(4)])
+#print Sum([(((x^y) & (2**(i)))/(2**(i))) for i in range(8)])
 #check that they all have the same number of bits after xor
 
 s.add(
-  Sum([(((x^y) & (2**(i)))/(2**(i))) for i in range(4)]) ==
-  Sum([(((y^z) & (2**(i)))/(2**(i))) for i in range(4)])
+  Sum([(((x^y) & (2**(i)))/(2**(i))) for i in range(8)]) ==
+  Sum([(((y^z) & (2**(i)))/(2**(i))) for i in range(8)])
 )
 s.add(
-  Sum([(((y^z) & (2**(i)))/(2**(i))) for i in range(4)]) ==
-  Sum([(((z^w) & (2**(i)))/(2**(i))) for i in range(4)])
+  Sum([(((y^z) & (2**(i)))/(2**(i))) for i in range(8)]) ==
+  Sum([(((z^w) & (2**(i)))/(2**(i))) for i in range(8)])
 )
 s.add(
-  Sum([(((z^w) & (2**(i)))/(2**(i))) for i in range(4)]) ==
-  Sum([(((w^p) & (2**(i)))/(2**(i))) for i in range(4)])
+  Sum([(((z^w) & (2**(i)))/(2**(i))) for i in range(8)]) ==
+  Sum([(((w^p) & (2**(i)))/(2**(i))) for i in range(8)])
 )
 s.add(
-  Sum([(((w^p) & (2**(i)))/(2**(i))) for i in range(4)]) ==
-  Sum([(((p^n) & (2**(i)))/(2**(i))) for i in range(4)])
+  Sum([(((w^p) & (2**(i)))/(2**(i))) for i in range(8)]) ==
+  Sum([(((p^n) & (2**(i)))/(2**(i))) for i in range(8)])
 )
 
 if(s.check() == sat):
