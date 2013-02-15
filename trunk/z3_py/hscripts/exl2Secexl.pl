0c9a7fa54fa87abeebd8f6a23d28d96a54ae8850
borowczak@gmail.com <borowczak@gmail.com@a7cb938e-b4f2-1df4-7820-3bc43eb7a654>
2013-02-15 19:33:35 +0000

    Corrected version of the secure state exl creator

--- trunk/z3_py/hscripts/exl2Secexl.pl
+++ trunk/z3_py/hscripts/exl2Secexl.pl
@@ -6,54 +6,104 @@ open(my $fh, '<:encoding(UTF-8)', $filename)
     or die "Could not open file '$filename' $!";
 
 my (%HoH); 
-
+my (@newStateStrings);
+my @data;    
+my $trans = 0;
+my $state = 0;
+my $inputlen, $outputlen;
 while (my $line = <$fh>) {
     $cline = $line;
-    chomp $cline;
-    if($cline =~ m/(\S+)(\s+)(\S+)(\s+)(\S+)(\s+)(\S+)/g ){
-	push@{$HoH{$3}{$5}}, ($1, $7);
+#    chomp $cline;
+    if($cline =~ m/(\S+|[\-]+)(\s+)(\S+)(\s+)(\S+)(\s+)(\S+)/g ){
+  	push(@data, ($3,$5,$1,$7));
+	$inputlen = $1;
+	$outputlen = $7;
+
+    }
+}
+
+my $flag = 0;
+my $currSourceState = "st1"; 
+my (@dest_list, @in_list, @out_list);
+
+for (my $idx = 0; $idx < $#data+1; $idx=$idx+4){
+    my ($source, $dest, $input, $output) = @data[$idx..$idx+3];
+    # print "($source, $dest, $input, $output) \n";
+    if($source eq $currSourceState){
+	#create matrix
+	if($source eq $dest){$flag = 1;}
+	push(@dest_list, $dest);
+	push(@in_list, $input);
+	push(@out_list, $output);
+    }
+    else{
+	$state++;
+	#new state check if we need to duplicate all the previous ones
+	
+	if($flag == 1){
+	    $state++;
+	    my $sourceLen = $#dest_list+1;
+	    my @dup_list = @dest_list;
+	    for(my $idx2 = 0; $idx2 < $sourceLen; $idx2++){
+		if($dest_list[$idx2] eq $currSourceState){
+		    $dest_list[$idx2] = $currSourceState."_n";
+		}
+		print "@in_list[$idx2]\t$currSourceState\t@dest_list[$idx2]\t@out_list[$idx2]\n";
+		$trans++;
+	    }
+	    for(my $idx2 = 0; $idx2 < $sourceLen; $idx2++){
+		print "@in_list[$idx2]\t".$currSourceState."_n\t@dup_list[$idx2]\t@out_list[$idx2]\n";
+		$trans++;
+	    }
+	    $flag = 0;
+	}
+	else{
+	    my $sourceLen = $#dest_list+1;
+	    for(my $idx2 = 0; $idx2 < $sourceLen; $idx2++){
+		print "@in_list[$idx2]\t$currSourceState\t@dest_list[$idx2]\t@out_list[$idx2]\n";
+		$trans++;
+	    }
+	}
+	
+	$currSourceState = $source;	
+	@dest_list =  $dest;
+	@in_list = $input;
+	@out_list, $output;
+	
+    }
+}
+
+$state++;
+#need to grab the last entry
+if($flag == 1){
+    $state++;
+    my $sourceLen = $#dest_list+1;
+    my @dup_list = @dest_list;
+    for(my $idx2 = 0; $idx2 < $sourceLen; $idx2++){
+	if($dest_list[$idx2] eq $currSourceState){
+	    $dest_list[$idx2] = $currSourceState."_n";
+	}
+	print "@in_list[$idx2]\t$currSourceState\t@dest_list[$idx2]\t@out_list[$idx2]\n";
+	$trans++;
+    }
+    for(my $idx2 = 0; $idx2 < $sourceLen; $idx2++){
+	print "@in_list[$idx2]\t".$currSourceState."_n\t@dup_list[$idx2]\t@out_list[$idx2]\n";
+	$trans++;
+    }
+    $flag = 0;
+}
+else{
+    my $sourceLen = $#dest_list+1;
+    for(my $idx2 = 0; $idx2 < $sourceLen; $idx2++){
+	print "@in_list[$idx2]\t$currSourceState\t@dest_list[$idx2]\t@out_list[$idx2]\n";
+	$trans++;
     }
 }
 
 
-$defaultxor;
-$count = $newcount = $origcount = 0;
-$tran = $newtran = $origtran = 0;
-my @array;
-for $source (sort keys %HoH) {
-    for $dest (sort keys %{ $HoH{$source} } ){
-	$origtran++;
-	if ($source eq $dest){ #cycle detected
-	    @arry = @{$HoH{$source}{$source}};
-	    delete($HoH{$source}{$source});
-	    $newSource = $source."_n";
-	    %{$HoH{$newSource}} = %{$HoH{$source}};	    
-	    push@{$HoH{$source}{$newSource}}, @arry;
-	    push@{$HoH{$newSource}{$source}}, @arry;
-	    $newcount = $newcount+1;
-	    $newtran = $newtran +1 + scalar(keys(%{$HoH{$source}})); #+2 -1 + count
-	} #end cycle fix
-    }
-    $origcount = $origcount+1;
-}
-
-$count = $origcount + $newcount;
-$tran = $origtran + $newtran;
-
-#print "#Total states is $count, including $origcount states and $newcount newstates\n";
-#print "#Total transisions is $tran, including $origtran transistions and $newtran new transitions\n";
-
-print ".i ".length($arry[0])."\n";
-print ".o ".length($arry[1])."\n";
-print ".p ".$tran."\n";
-print ".s ".$count."\n\n\n";
-
-
-
-for $source (sort keys %HoH) {
-    for $dest (sort keys %{ $HoH{$source} } ){
-	print "$HoH{$source}{$dest}[0]\t$source\t$dest\t$HoH{$source}{$dest}[1]\n";
-    }
-}
+print ".i ".length($inputlen)."\n";
+print ".o ".length($outputlen)."\n";
+print ".p ".$trans."\n";
+print ".s ".$state."\n\n\n";
 print ".e \n";
 
