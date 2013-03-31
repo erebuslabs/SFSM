3deac95109ee324f7a25e86c55990ac15e943acd
borowczak@gmail.com <borowczak@gmail.com>
2013-03-31 22:56:42 +0000

    modifcations to batch scripts, key changes allow parameterized calling of z3 generation script

--- z3_py/createSecure.bash
+++ z3_py/createSecure.bash
@@ -1,7 +1,8 @@
 #/usr/bin/bash
 
+
 for file in $@; do
-    base=$file;#"/data.exl" ;
+    base=$file"/data.exl" ;
     secfile=$file"_s.exl";
     gv=$file".gv";
     sgv=$file"_s.gv";
@@ -12,34 +13,37 @@ for file in $@; do
     echo " "
     echo "v=v=v==========$base=============v=v=v"
     echo "Converting the $base file to secure $secfile";
-    ./hscripts/exl2Secexl.pl $base > $secfile
+#    ./hscripts/exl2Secexl.pl $base > $secfile
     echo "Converting both files to dot file";
     echo "=Converting the $base file to $gv";
-    ./hscripts/exl2dot.pl $base > $gv;
+#    ./hscripts/exl2dot.pl $base > $gv;
     echo "=Converting the $secfile file to  $sgv";
-    ./hscripts/exl2dot.pl $secfile > $sgv;
+#    ./hscripts/exl2dot.pl $secfile > $sgv;
     echo "Converting $gv to $dot";
-   neato -Tpng $gv -o $dot 
+#   neato -Tpng $gv -o $dot 
 
     echo "Converting $sgv to $sdot";
-    dot -Tpng $sgv -o $sdot 
+#    dot -Tpng $sgv -o $sdot 
 
- 
 
-#    echo "moving $secfile bmarks/secure/.";
-#    mv $secfile bmarks/secure/.;
+    fname=$(echo $base | cut -d'/' -f2)
+    echo "copying $base bmarks/orig/$fname.exl";
+    cp $base bmarks/orig/$fname.exl
 
-#    echo "moving $dot results/viz/gv/.";
-#    mv $gv results/viz/gv/.;
-#
-#    echo "moving $sdot results/viz/gv/.";
-#    mv $sgv results/viz/gv/.;
+    echo "moving $secfile bmarks/secure/.";
+    mv $secfile bmarks/secure/.;
 
-#    echo "moving $dot results/viz/gv/.";
-#    mv $dot results/viz/png/.;
+    echo "moving $dot results/viz/gv/.";
+    mv $gv results/viz/gv/.;
 
-#    echo "moving $sdot results/viz/gv/.";
-#    mv $sdot results/viz/png/.;
+    echo "moving $sdot results/viz/gv/.";
+    mv $sgv results/viz/gv/.;
+
+    echo "moving $dot results/viz/gv/.";
+    mv $dot results/viz/png/.;
+
+    echo "moving $sdot results/viz/gv/.";
+    mv $sdot results/viz/png/.;
 
     echo "^=^=^==========$base=============^=^=^"
     echo " "
--- z3_py/runAll.bash
+++ z3_py/runAll.bash
@@ -2,24 +2,40 @@
 
 for file in $@; do
 
-    base=$file;#"/data.exl" ;
-    pythres=$file"_hd4.py"; 
-    satres=$file"_hd4.out";
-    final=$file"_hd4.csv";
+    base=$file;
+
+    search_dist=4;
+    hd_on=1;
+    hw_on=1;
+    hd_val=0;
+    hw_val=0;
+    
+    
+    optstring="_hw_"$hw_on"_hd_"$hd_on;
+    $optstring =  ${optstring//1/on}   
+    $optstring =  ${optstring//0/off}  
+    $optstring .= "hwVal_"$hw_val"_hdVal_"$hd_val;
+    $optstring =  ${optstring//0/equal}
+
+    pythres=$file$optstring"_.py"; 
+    
+    satres=$file$optstring".out";
+    final=$file$optstring".csv";
+
     echo " "
     echo "v=v=v==========$base=============v=v=v"
     echo "Converting the $base file";
-    ./hscripts/exlz3py.pl $base > $pythres
+    ./hscripts/exlz3py.pl $base $search_dist $hw_on $hd_on $hw_val $hd_val > $pythres
     echo "Running Z3 theorem prover on $pythres";
     (time (timeout 1600 python $pythres > $satres;)) 2>&1
     echo "Converting result to binary";
     ./hscripts/z3toBin.pl $satres > $final;
-#    echo "moving $pythres isvlis/results/python/.";
-#    mv $pythres isvlsi/results/python/.;
-#    echo "moving $satres results/raw/.";
-#    mv $satres isvlsi/results/raw/.;
-#    echo "moving $final results/final/.";
-#    mv $final isvlsi/results/final/.;
+    echo "moving $pythres results/python/.";
+    mv $pythres results/python/.;
+    echo "moving $satres results/raw/.";
+    mv $satres results/raw/.;
+    echo "moving $final results/final/.";
+    mv $final results/final/.;
     echo "^=^=^==========$base=============^=^=^"
     echo " "
 done
