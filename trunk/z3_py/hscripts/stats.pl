03c78ea24c52c9f462332ba6f99eadc9de3d14da
borowczak@gmail.com <borowczak@gmail.com@a7cb938e-b4f2-1df4-7820-3bc43eb7a654>
2013-05-26 23:57:57 +0000

    Modifications to stats

--- trunk/z3_py/hscripts/stats.pl
+++ trunk/z3_py/hscripts/stats.pl
@@ -1,10 +1,16 @@
 #!/usr/bin/perl -w
+#use strict;
+
 
 use POSIX;
 use Statistics::Descriptive;
 use Graphics::GnuplotIF qw(GnuplotIF);
 use List::Util qw(sum);
 use List::MoreUtils qw(uniq);
+#use Math::GSL::Statistics qw /:all/;
+#use PDL::Stats::Basic;
+
+use Statistics::Basic qw(:all nofill);
 
 my $localstats = Statistics::Descriptive::Full->new();
 
@@ -30,7 +36,7 @@ my $idx = 0;
 my $VecCnt = 0;
 open(my $dfm, $datfile) or die("ack error - $!");
 
-my(@maxcurr);
+my(@maxcurr, @varcurr);
 
 while(<$dfm>){
     if(m/^(\d+)\s+([-\d]+)\s*$/ && $idx > $delayEnd){
@@ -44,6 +50,7 @@ while(<$dfm>){
 	    #e.g. MAX curr 
 	    $localstats->add_data(@data);
 	    push @maxcurr, $localstats->max(); 
+	    push @varcurr, $localstats->standard_deviation();
 	    #push @meancurr, $localstats->mean(); 
 	    push @row, [@data];    
 	    delete @data[0..$#data];
@@ -55,11 +62,17 @@ while(<$dfm>){
 close $dfm;
 
 #Compute Agregate Stats for all entries
-print "\nCurrent Statisticts n=",($VecCnt-1);
+print "\nStatisticts n=",($VecCnt-1);
 $localstats->add_data(@maxcurr);
-print "\nmax,",$localstats->max();  
-print "\nmean max,",$localstats->mean(); 
-print "\nvar max,",$localstats->variance(); 
+print "\nmax curr,",$localstats->max(),",@",$localstats->maxdex();  
+print "\nmean max curr,",$localstats->mean(); 
+print "\nvar max curr,",$localstats->variance(); 
+$localstats->clear();
+
+$localstats->add_data(@varcurr);
+print "\nmax variation,",$localstats->max(),",@",$localstats->maxdex();    
+print "\nmean variation,",$localstats->mean(); 
+print "\nvar variation,",$localstats->variance(); 
 
 $localstats->clear();
 #$localstats->add_data(@meancurr);
@@ -100,7 +113,7 @@ else{
 }
 
 
-my (@modMeans, @modStdDev, @modVariance); 
+my (@modMeans, @modStdDev, @modVariance, @modMin, @modMax, @modUniq); 
 my @temp = ();
 foreach $model (@Models){
     my @tempdata = @{$model};
@@ -108,46 +121,16 @@ foreach $model (@Models){
     push(@modMeans, $localstats->mean());
     push(@modVariance, $localstats->variance());
     push(@modStdDev, $localstats->standard_deviation());
+    push(@modMin, $localstats->min());
+    push(@modMax, $localstats->max());
+    push(@modUniq, scalar(uniq(@tempdata)));
     $localstats->clear();
     #dynamic array of arrays creation
     push(@modelCorr, [@temp]);
 }
 
-my $plotvector = Graphics::GnuplotIF->new(title => "Original Current Source",
-					  ylabel=> 'Rounds',
-					  xlabel=> 'Current ',
-					  style => "pm3d",
-					  perrsist   => 1,
-					  plot_also  => 1,
-					  scriptfile => '3dplot.cmds',
-);
 
-$plotvector->gnuplot_cmd(
-    'set pm3d',
-#			 'set palette rgbformulae 34,35,36',
-#			 'set palette rgbformulae 30,31,32',
-    'set palette gray',
-    'set ticslevel 0',
-    'set dgrid3d 100,50',
-  #  'set grid 500,50',
-    'set isosample 100,100',
-#			 'set key center bottom',
-    'unset key', 
-#			 'set linestyle 1 lt 2 lw 3',
-#			 'set key box linestyle 1'
-    );
-
-$plotvector->gnuplot_hardcopy( '3dplot.ps',
-                            'postscript color',
-                            'lw 2' );
-#$plotvector->gnuplot_cmd( 'set terminal png ',#
-#			  'set output "3dplot.png" ' );
-
-$plotvector->gnuplot_set_plot_titles("Current Trace Per Round");
-$plotvector->gnuplot_plot_3d(\@row);
-#$plotvector->gnuplot_pause(10);
-
-
+print3D(@row);
 
 my (@mean, @variance, @std_dev, @corrdp);
 my @allDPVecs;
@@ -172,13 +155,13 @@ foreach $dpidx (0..($sampleRate*$clkperiod)-1){
     $localstats->clear();
     
     #compute the moment of the datapoint vector
-    my @PMoment_dpvec = map {$_ - $dpvec_mean } @dpvec;
+    #my @PMoment_dpvec = map {$_ - $dpvec_mean } @dpvec;
     
     $model_idx = 0;
     #compute the correlation for against each model
     foreach $model (@Models){
 	#compute the moment of the model
-	@PMoment_model = map{ $_ - $modMeans[$model_idx]}@{$model};;
+#	@PMoment_model = map{ $_ - $modMeans[$model_idx]}@{$model};;
 	
 #	print "@{$model} <--model\n";
 #	print "@PMoment_model <--Moment model \n";
@@ -188,18 +171,32 @@ foreach $dpidx (0..($sampleRate*$clkperiod)-1){
 	##    ---------------------------------    ##
 	##      std_dev_dpvec x std_dev_model      ##
 
-	my $numerator = sum(map{($PMoment_dpvec[$_] * $PMoment_model[$_])/$#PMoment_dpvec} 0..$#PMoment_dpvec);
-	my $denomenator = ($dpvec_std_dev*$modStdDev[$model_idx]);
+#	my $numerator = sum(map{($PMoment_dpvec[$_] * $PMoment_model[$_])/$#PMoment_dpvec} 0..$#PMoment_dpvec);
+#	my $denomenator = ($dpvec_std_dev*$modStdDev[$model_idx]);
 
 #	print "\n ($numerator)  $dpvec_std_dev * $modStdDev[$model_idx])\n";
 
-	my $pearsons=0;
-	if($denomenator!=0){
-	     $pearsons = ($numerator)/$denomenator;
-	}
-	push(@{$modelCorr[$model_idx]}, $pearsons);
+	#my $pearsons=0;
+	#if($denomenator!=0){
+	#     $pearsons = ($numerator)/$denomenator;
+	#}
 
-	#print "For this model at dp# $dpidx: corr=($numerator)/($denomenator)=$pearsons\n";
+#	print  "V#######\nArrayMODEL\n#####V";
+#	print @$model;
+#	print  "V#######\nArrayDPVEC\n#####V";
+#	print @dpvec ;
+	my $pearsons = 0;
+	if(stddev(\@$model) != 0 && stddev(\@dpvec)!= 0)
+	{	
+	    $pearsons = correlation(\@$model,\@dpvec);
+	}
+
+#gsl_stats_mean($model,1,$#model+1);#
+
+#	print "\nOld ".$pearsons;#." New ".$npearsons; 
+	push(@{$modelCorr[$model_idx]}, abs($pearsons));
+
+#	print "For this model at dp# $dpidx: corr=($numerator)/($denomenator)=$pearsons\n";
 	$model_idx++;
     }    
     push (@allDPVecs, [@dpvec]);
@@ -238,26 +235,32 @@ foreach $model (@Models){
 	.$modelName[$modelidx]
 	." and Datapoints at t=".$maxcorridx; 
 
-    my $plot1 = Graphics::GnuplotIF->new(title => $title,
-					 style => "points", ylabel=> 'Curr',
-					 y2label=> 'Model',
-					 xlabel=> 'Rounds ',
-					 perrsist    => 1,
-					 plot_also   => 1,
-					 scriptfile  => "${modelidx}_plot.cmds"
+    my $plot1 = Graphics::GnuplotIF->new(
+	title => $title,
+	style => "points", ylabel=> 'Curr',
+	y2label=> 'Model',
+	xlabel=> 'Rounds ',
+	perrsist    => 1,
+	plot_also   => 1,
+	scriptfile  => "${modelidx}_plot.cmds"
 	) ;
 
 
-    my @activedpvector = @{$allDPVecs[$maxcorridx]};
+
     $localstats->clear();
+    $maxcorridx = 591;
+
+    my @activedpvector = @{$allDPVecs[$maxcorridx]};
     $localstats->add_data(@activedpvector);
     my $DPMAX = $localstats->max();
     my $DPMIN = $localstats->min();
   
 
+    my @steppedarray = 
+	map{ceil(($_-$DPMIN)/(($DPMAX-$DPMIN)/$MODELUNI)/1)} @activedpvector;
 
-    my @steppedarray = map{ceil(($_-$DPMIN)/(($DPMAX-$DPMIN)/$MODELUNI)/1)} @activedpvector;
-
+    #Compute the correlation between binned array and model
+    
 
     $plot1->gnuplot_cmd('set y2label "Model"',
 			"set y2range [$MODELMIN:$MODELMAX]",
@@ -271,17 +274,20 @@ foreach $model (@Models){
 
     
     #print out model and the data vector it correlated too
-    
+
+    #Create a generic X-axis
     my @x = [0 .. $#activedpvector-1];
 
-
+    #create hashes
     my %activedp = ('x_values'=> @x, 
 		    'y_values'=>\@activedpvector, 
 		    'style_spec' => "lines axes x1y1"); 
+
     my %model1 = ('x_values'=>@x,
 		  'y_values'=>\@{$model},
 		  'style_spec' => "lines axes x1y2");
     
+    #Normalized DP vector
     my %binned =  ('x_values'=>@x,
 		  'y_values'=>\@steppedarray,
 		  'style_spec' => "lines axes x1y2");
@@ -307,7 +313,7 @@ foreach $model (@Models){
   
     #%freq = $localstats->frequency_distribution($MODELUNI);
     #print $localstats->frequency_distribution();
-    print "\n";
+
 
 #    for (sort {$a <=> $b} keys %freq) {
 #	print "key = $_, count = $freq{$_}\n";
@@ -329,33 +335,43 @@ foreach $model (@Models){
 
 
     $modelidx++;
+    print "\n";
 }
-  
-#$plot1->gnuplot_set_plot_titles("Mean");
-#$plot1->gnuplot_plot_y( \@mean );                
-#$plot1->gnuplot_pause(10 );
 
-
-
-
-#$plot1->gnuplot_set_plot_titles("Variance");
-#$plot1->gnuplot_plot_y( \@row );            
-#$plot1->gnuplot_pause(10 );
-
-#$plot1->gnuplot_set_plot_titles("STD DEV");
-#$plot1->gnuplot_plot_y( \@std_dev );            
-#$plot1->gnuplot_pause(10 );
-
-
-#my  $plotF = Graphics::GnuplotIF->new(title => "line", style => "points", plot_titles =>["State", "tran", "HW", "HD"]);
-
-#$plotF->gnuplot_cmd('set grid',
-#		    'set key outside bottom',
-#		    'set linestyle 1 lt 2 lw 3',
-#		    'set key box linestyle 1'
-#    );
-#$plotF->gnuplot_plot_y(@modelCorr);
-#$plotF->gnuplot_pause(30 );
-
+sub print3D{ 
+    my $plotvector = Graphics::GnuplotIF->new(
+	title => "Original Current Source",
+	ylabel=> 'Rounds',
+	xlabel=> 'Time\n (normalized, unitless)',
+	zlabel=> 'Current ',
+#1	style => "pm3d",
+	style => "lines",
+	perrsist   => 1,
+	plot_also  => 1,
+	scriptfile => '3dplot.cmds',
+	);
+$plotvector->gnuplot_cmd(
+    'set zlabel "i"',
+    'set hidden3d',
+    'set palette gray',
+    'set ticslevel 0',
+    'set dgrid3d 30,50',
+    #'set grid 0 10 ',
+    'set view 70,10',
+    'set isosample 100,100',
+    'unset key', 
+    'set contour base'
+    );
+$plotvector->gnuplot_hardcopy( 
+    '3dplot.ps',
+    'postscript color',
+    'lw 2' );
     
+    $plotvector->gnuplot_set_plot_titles("Current Trace Per Round");
+    $plotvector->gnuplot_plot_3d(\@_);
+
+}
+    
+
+
 
