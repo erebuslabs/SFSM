7e80da0293421523f8b29877f2bb761d8b4e505e
borowczak@gmail.com <borowczak@gmail.com@a7cb938e-b4f2-1df4-7820-3bc43eb7a654>
2013-09-07 17:11:31 +0000

    pushing old updates through

--- trunk/z3_py/hscripts/genFSMBen.pl
+++ trunk/z3_py/hscripts/genFSMBen.pl
@@ -1,28 +1,86 @@
 #!/usr/bin/perl
 
 use POSIX qw(ceil);
+use List::Util qw(sum);
 
-my ($minstates, $maxstates, $stepstate) = (shift, shift, shift);
+my ($states, $slpercent, $connectfeq) = (shift, shift, shift);
 
+my @connectMatrix;
 
-for (my $s_itt = $minstates; $s_itt <= $maxstates; $s_itt+=$stepstate){
+my @diag = rndvec($slpercent, $states);
+my @final;
+my $globmax = 0;
 
-    print "\n***Creating FSM Family with $s_itt States\n";
-    ###GENERATE $s_itt states###
-    for (my $selfloop_itt =  0; $selfloop_itt <= $s_itt ; $selfloop_itt=ceil($selfloop_itt+($s_itt/100))) {
-	
-	###SELECT $selfloop_itt states to create self loops###
+my $out ="";
 
-	###Itterate through available population Schemes: MST to Fully Connected
-	###POPULATE Connection Matrix####
-	for(my $con_itt = $s_itt-1 ; $con_itt <= ($s_itt*(s_itt+1))/2; $con_itt+=$s_itt){
-	    print "\n***Creating FSM Family with $s_itt States; $selfloop_itt self loops and $con_itt connections\n";
+for(my $ridx = 0; $ridx < $states; $ridx++){
+    my @row;
+    my @trow = rndvec($connectfeq, $states-1); #(0) x $states;
+    push @row, @trow[0..($ridx-1)];
+    push @row, "0" ;
+    push @row, @trow[($ridx)..($states-1)];
+    $row[rand($ridx)] = 1;
+    $row[$ridx] = $diag[$ridx];
+    my $lmax = sum(@row);
+    if($lmax > $globmax){
+	$globmax = $lmax;
+    }
+    push(@final, [@row]);
+}
 
+my $srcidx = 0; 
+my $tcount = 0;
+
+foreach $row (@final){
+    my $desidx = 0; #$states-1;    
+    my $curr = 0;
+    foreach $elm (@{$row}){
+	if($elm){
+	    $out .= ohdecide($curr, $globmax);
+	    $out .= " st${srcidx} st${desidx} ";
+	    $out .= dec2bin(($desidx+$srcidx), ceil(log($states)/log(2)));
+	    $out .= "\n";
+	    $curr++;
+	    $tcount++;
 	}
-    } 
+	$desidx++;
+    }
+    $srcidx++;
+}
+
+print ".i $globmax\n";
+print ".o ",ceil(log($states)/log(2)),"\n";
+print ".p $tcount\n";
+print ".s $states\n";
+print $out;
+
+sub ohdecide{
+    my ($loc, $gmax) = (shift, shift);
+    my @temp = ("-")x$gmax;
+    $temp[$loc] = 1;
+    return join("",@temp);
 
 }
 
+sub dec2bin { 
+    my ($val, $idx) = (shift, shift);
+    my $str = unpack("B32", pack("N", $val)); 
+    return substr $str, (-1*$idx);
+}
 
+sub bineqv{
+    my $num = (shift);
+    return sprintf "%b", $num;
+}
 
-
+#Takes % of self loops and number of states,
+#returns vector of 1s,0s - connected/not connected
+sub rndvec{
+    my ($lperc, $length) = (shift, shift);
+    $lperc = $lperc/100;
+    my $onbits = sprintf("%.0f",$lperc*$length);
+    do{
+	@vec = map{ int(rand() < $lperc) } (1..$length);
+    }while(sum(@vec) != $onbits);
+    return @vec;
+}
